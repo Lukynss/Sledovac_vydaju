e8a65a739ed617020edc8f3c0ac8e9b74ff97c6a
Mikoláš Pešek <mikolas.pesek@educanet.cz>
2026-01-05 15:35:24 +0100

    updated &working

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import Header from "./components/Header.js"
 import Tasks from "./components/Tasks";
 import Task from "./components/Task";
 
+import {useState, useEffect} from "react";
+
 function App() {
-    const Tasks = [
-        {
-        id: 1,
-        name: "Vynést koše",
-        description: "Už smrdí"
-        },
-        {
-        id: 2,
-        name: "Umýt okna",
-        description: "Není vidět ven"
-        }
-    ]
+    const [tasks, setTasks] = useState(
+        [
+            {
+                id: 1,
+                name: "Vynést koše",
+                description: "Už smrdí"
+            },
+            {
+                id: 2,
+                name: "Umýt okna",
+                description: "Není vidět ven"
+            }
+        ]
+    )
 
     const addTask = (newTask) => {
         setTasks([...tasks, newTask]);
