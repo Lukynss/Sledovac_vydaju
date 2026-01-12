e7187e02acae20c614690d12e9a8cabf22705b20
Mikoláš Pešek <mikolas.pesek@educanet.cz>
2026-01-12 15:10:01 +0100

    fix

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -13,8 +13,8 @@ function AddTask({onSubmit}) {
     return (
         <div className="">
             <form action="" className="form d-flex flex-column align-items-start" onSubmit={submit}>
-                <input type="text" className="form-control mb-3" placeholder="Název úkolu" onChange={(e) => {setName(e.target.value)}} />
-                <input type="text" className="form-control mb-3" placeholder="Popis úkolu" onChange={(e) => {setDescription(e.target.value)}} />
+                <input type="text" className="form-control mb-3" placeholder="Název úkolu" value={name} onChange={(e) => {setName(e.target.value)}} />
+                <input type="text" className="form-control mb-3" placeholder="Popis úkolu" value={description} onChange={(e) => {setDescription(e.target.value)}} />
                 <input type="submit" value="Přidat úkol" className="btn btn-success"/>
             </form>
         </div>
