d2a3cab8e0a78d4e643d404a7de5af3aae71888d
Mikoláš Pešek <mikolas.pesek@educanet.cz>
2026-01-12 15:29:22 +0100

    Základní routing

--- src/App.js
+++ src/App.js
@@ -2,42 +2,27 @@ import './App.css';
 import Header from "./components/Header.js"
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-
 import {useState, useEffect} from "react";
+import Home from "./pages/Home";
+import Todolist from "./pages/Todolist";
+
+import {Link, Routes, Route, Router} from "react-router";
 
 function App() {
-    const [tasks, setTasks] = useState(
-        [
-            {
-                id: 1,
-                name: "Vynést koše",
-                description: "Už smrdí"
-            },
-            {
-                id: 2,
-                name: "Umýt okna",
-                description: "Není vidět ven"
-            }
-        ]
+    return (
+        <article>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/todo">To-do list</Link>
+            </nav>
+
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/todo" element={<Todolist />}/>
+                </Routes>
+        </article>
     )
 
-    const addTask = (newTask) => {
-        let newId = Math.max(...tasks.map(task => task.id)) + 1;
-        newTask.id = newId;
-        setTasks([...tasks, newTask]);
-    }
-
-    const removeTask = (id) => {
-        setTasks(tasks.filter((task) => task.id !== id));
-    }
-
-  return (
-    <div className="App container p-5">
-        <Header text="To-do List" className="mb-3 mt-3"/>
-        <AddTask onSubmit={addTask} />
-        <Tasks tasks={tasks} Delete={removeTask} />
-    </div>
-  );
 }
 
 export default App;
---
+++ src/pages/Todolist.js
@@ -0,0 +1,41 @@
+import {useState} from "react";
+import Header from "../components/Header";
+import AddTask from "../components/AddTask";
+import Tasks from "../components/Tasks";
+
+function Todolist () {
+    const [tasks, setTasks] = useState(
+        [
+            {
+                id: 1,
+                name: "Vynést koše",
+                description: "Už smrdí"
+            },
+            {
+                id: 2,
+                name: "Umýt okna",
+                description: "Není vidět ven"
+            }
+        ]
+    )
+
+    const addTask = (newTask) => {
+        let newId = Math.max(...tasks.map(task => task.id)) + 1;
+        newTask.id = newId;
+        setTasks([...tasks, newTask]);
+    }
+
+    const removeTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+    }
+
+    return (
+        <div className="App container p-5">
+            <Header text="To-do List" className="mb-3 mt-3"/>
+            <AddTask onSubmit={addTask} />
+            <Tasks tasks={tasks} Delete={removeTask} />
+        </div>
+    );
+}
+
+export default Todolist;
